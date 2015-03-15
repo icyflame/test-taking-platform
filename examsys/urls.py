0adf184e841bedffa118e85eda94ff099862cb6f
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-03-15 23:56:50 +0530

    Add the choose test and take test to URLs

--- examsys/urls.py
+++ examsys/urls.py
@@ -8,4 +8,7 @@ urlpatterns = patterns('',
     url(r'^login/', views.login, name='login'),
     url(r'^logout/', views.logout, name='logout'),
     url(r'^register/', views.register, name='register'),
+    url(r'^choosetest/', views.choosetest, name='choosetest'),
+    url(r'^(?P<test_id>\d+)/$', views.taketest, name='taketest'),
+    
 )
