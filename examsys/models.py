be05957cbd010c368386a12afe7ca72770cd219c
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-03-15 23:03:33 +0530

    Add the question, test and user models

--- examsys/models.py
+++ examsys/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+	qtext = models.CharField(max_length=200)
+	correctans = models.CharField(max_length=200)
+
+class User(models.Model):
+	name = models.CharField(max_length=200)
+	password = models.CharField(max_length=200)
+	username = models.CharField(max_length=30)
+
+class Test(models.Model):
+	tname = models.CharField(max_length=200)
+
+class TestToAnswer(models.Model):
+	qid = models.ForeignKey(Question)
+	tid = models.ForeignKey(Test)
